d39b42e5a24eff76efb8a528086e800badb6d635
Ignacio <carlos.zepeda.654@gmail.com>
2023-07-25 09:41:18 -0600

    portfolio added

--- js/main.js
+++ js/main.js
@@ -71,3 +71,42 @@ tabs.forEach(tab => {
         tab.classList.add('qualification__active')
     })
 })
+
+/*==================== SERVICES MODALS ====================*/
+
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close');
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+};//Function click
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    })
+});
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        })
+    })
+})
+
+/*==================== Swiper ====================*/
+let swiperPortfolio = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+})
